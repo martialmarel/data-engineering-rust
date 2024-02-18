07691c4c1fcc99c6e9c19bc8bab23e04397d3b8d
Martial <martial@marels.fr>
2024-02-18 20:55:39 +0100

    modify program th have CLI and pretty terminal display

--- week-03/csv-reader/src/main.rs
+++ week-03/csv-reader/src/main.rs
@@ -1,19 +1,58 @@
-use std::{error::Error, io, process};
+use clap::Parser;
+use comfy_table::Table;
+use std::{error::Error, path::Path, process};
 
-fn example() -> Result<(), Box<dyn Error>> {
-    // Build the CSV reader and iterate over each record.
-    let mut rdr = csv::Reader::from_reader(io::stdin());
-    for result in rdr.records() {
-        // The iterator yields Result<StringRecord, Error>, so we check the
-        // error here..
-        let record = result?;
-        println!("{:?}", record);
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// The path to the CSV file to read
+    #[arg(value_name = "CSV filepath")]
+    csv_filepath: String,
+
+    /// The delimiter used in the CSV file
+    #[arg(short, long, default_value = "';'")]
+    delimiter: char,
+
+    /// Whether the CSV file has headers
+    #[arg(short, long, action = clap::ArgAction::Set, default_value = "true")]
+    with_headers: String,
+}
+
+fn read_csvfile(
+    filepath: String,
+    delimiter: char,
+    with_headers: bool,
+) -> Result<(), Box<dyn Error>> {
+    let mut rdr = csv::ReaderBuilder::new()
+        .delimiter(delimiter as u8)
+        .from_path(filepath)?;
+    let mut table = Table::new();
+
+    if with_headers && rdr.headers().is_ok() {
+        table.set_header(rdr.headers()?.iter());
     }
+
+    for result in rdr.records() {
+        let record = result?;
+        table.add_row(&record);
+    }
+
+    println!("{table}");
     Ok(())
 }
 
 fn main() {
-    if let Err(err) = example() {
+    let cli = Cli::parse();
+    let csv_filepath = cli.csv_filepath;
+    println!("Processing CSV: {}", csv_filepath);
+
+    if !Path::new(&csv_filepath).exists() {
+        panic!("File does not exist: {}", csv_filepath);
+    }
+
+    let csv_with_headers = matches!(cli.with_headers.as_str(), "yes" | "true" | "1");
+
+    if let Err(err) = read_csvfile(csv_filepath, cli.delimiter, csv_with_headers) {
         println!("error running example: {}", err);
         process::exit(1);
     }
